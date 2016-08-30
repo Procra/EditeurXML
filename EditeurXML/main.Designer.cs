1c5f8beaa0ad62d6695a0566cbef2bbaec517945
Stephen <stephen.gicquel@live.fr>
2016-08-30 09:29:21 +0200

    Add files via upload

---
+++ EditeurXML/main.Designer.cs
@@ -0,0 +1,239 @@
+﻿namespace EditeurXML
+{
+    partial class main
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.buttonCharger = new System.Windows.Forms.Button();
+            this.treeViewXML = new System.Windows.Forms.TreeView();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItemCreer = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemRenommer = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItemSupprimer = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonParcourir = new System.Windows.Forms.Button();
+            this.textBoxUrl = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorkerLoad = new System.ComponentModel.BackgroundWorker();
+            this.labelnbnoeud = new System.Windows.Forms.Label();
+            this.labelnbnoeudxml = new System.Windows.Forms.Label();
+            this.labelsur = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonCharger
+            // 
+            this.buttonCharger.Location = new System.Drawing.Point(112, 19);
+            this.buttonCharger.Name = "buttonCharger";
+            this.buttonCharger.Size = new System.Drawing.Size(84, 23);
+            this.buttonCharger.TabIndex = 0;
+            this.buttonCharger.Text = "Charger";
+            this.buttonCharger.UseVisualStyleBackColor = true;
+            this.buttonCharger.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // treeViewXML
+            // 
+            this.treeViewXML.ContextMenuStrip = this.contextMenuStrip1;
+            this.treeViewXML.LabelEdit = true;
+            this.treeViewXML.Location = new System.Drawing.Point(6, 51);
+            this.treeViewXML.Name = "treeViewXML";
+            this.treeViewXML.Size = new System.Drawing.Size(312, 163);
+            this.treeViewXML.TabIndex = 1;
+            this.treeViewXML.MouseUp += new System.Windows.Forms.MouseEventHandler(this.treeViewXML_MouseUp);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItemCreer,
+            this.toolStripMenuItemRenommer,
+            this.toolStripMenuItemSupprimer});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(134, 70);
+            // 
+            // toolStripMenuItemCreer
+            // 
+            this.toolStripMenuItemCreer.Name = "toolStripMenuItemCreer";
+            this.toolStripMenuItemCreer.Size = new System.Drawing.Size(133, 22);
+            this.toolStripMenuItemCreer.Text = "Créer";
+            this.toolStripMenuItemCreer.Click += new System.EventHandler(this.toolStripMenuItemCreer_Click);
+            // 
+            // toolStripMenuItemRenommer
+            // 
+            this.toolStripMenuItemRenommer.Name = "toolStripMenuItemRenommer";
+            this.toolStripMenuItemRenommer.Size = new System.Drawing.Size(133, 22);
+            this.toolStripMenuItemRenommer.Text = "Renommer";
+            this.toolStripMenuItemRenommer.Click += new System.EventHandler(this.toolStripMenuItemRenommer_Click);
+            // 
+            // toolStripMenuItemSupprimer
+            // 
+            this.toolStripMenuItemSupprimer.Name = "toolStripMenuItemSupprimer";
+            this.toolStripMenuItemSupprimer.Size = new System.Drawing.Size(133, 22);
+            this.toolStripMenuItemSupprimer.Text = "Supprimer";
+            this.toolStripMenuItemSupprimer.Click += new System.EventHandler(this.toolStripMenuItemSupprimer_Click);
+            // 
+            // buttonParcourir
+            // 
+            this.buttonParcourir.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonParcourir.Location = new System.Drawing.Point(615, 23);
+            this.buttonParcourir.Name = "buttonParcourir";
+            this.buttonParcourir.Size = new System.Drawing.Size(75, 23);
+            this.buttonParcourir.TabIndex = 2;
+            this.buttonParcourir.Text = "Parcourir";
+            this.buttonParcourir.UseVisualStyleBackColor = true;
+            this.buttonParcourir.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBoxUrl
+            // 
+            this.textBoxUrl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxUrl.Location = new System.Drawing.Point(12, 25);
+            this.textBoxUrl.Name = "textBoxUrl";
+            this.textBoxUrl.Size = new System.Drawing.Size(597, 20);
+            this.textBoxUrl.TabIndex = 3;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(6, 235);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(312, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // backgroundWorkerLoad
+            // 
+            this.backgroundWorkerLoad.WorkerReportsProgress = true;
+            this.backgroundWorkerLoad.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorkerLoad_DoWork);
+            this.backgroundWorkerLoad.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorkerLoad_RunWorkerCompleted);
+            this.backgroundWorkerLoad.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorkerLoad_ProgressChanged);
+            // 
+            // labelnbnoeud
+            // 
+            this.labelnbnoeud.AutoSize = true;
+            this.labelnbnoeud.Location = new System.Drawing.Point(120, 261);
+            this.labelnbnoeud.Name = "labelnbnoeud";
+            this.labelnbnoeud.Size = new System.Drawing.Size(0, 13);
+            this.labelnbnoeud.TabIndex = 7;
+            // 
+            // labelnbnoeudxml
+            // 
+            this.labelnbnoeudxml.AutoSize = true;
+            this.labelnbnoeudxml.Location = new System.Drawing.Point(178, 261);
+            this.labelnbnoeudxml.Name = "labelnbnoeudxml";
+            this.labelnbnoeudxml.Size = new System.Drawing.Size(0, 13);
+            this.labelnbnoeudxml.TabIndex = 8;
+            // 
+            // labelsur
+            // 
+            this.labelsur.AutoSize = true;
+            this.labelsur.Location = new System.Drawing.Point(151, 261);
+            this.labelsur.Name = "labelsur";
+            this.labelsur.Size = new System.Drawing.Size(21, 13);
+            this.labelsur.TabIndex = 9;
+            this.labelsur.Text = "sur";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(324, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Précisez le chemin d\'accès du fichier XML à éditer puis chargez le :";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.labelnbnoeudxml);
+            this.groupBox1.Controls.Add(this.progressBar1);
+            this.groupBox1.Controls.Add(this.labelsur);
+            this.groupBox1.Controls.Add(this.treeViewXML);
+            this.groupBox1.Controls.Add(this.labelnbnoeud);
+            this.groupBox1.Controls.Add(this.buttonCharger);
+            this.groupBox1.Location = new System.Drawing.Point(12, 66);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(324, 277);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Edition";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(366, 66);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(324, 277);
+            this.groupBox2.TabIndex = 12;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Autre";
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 358);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxUrl);
+            this.Controls.Add(this.buttonParcourir);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "main";
+            this.Text = "EditeurXML";
+            this.Load += new System.EventHandler(this.main_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCharger;
+        private System.Windows.Forms.TreeView treeViewXML;
+        private System.Windows.Forms.Button buttonParcourir;
+        private System.Windows.Forms.TextBox textBoxUrl;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemCreer;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemRenommer;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItemSupprimer;
+        private System.ComponentModel.BackgroundWorker backgroundWorkerLoad;
+        private System.Windows.Forms.Label labelnbnoeud;
+        private System.Windows.Forms.Label labelnbnoeudxml;
+        private System.Windows.Forms.Label labelsur;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+
+    }
+}
+
---
+++ EditeurXML/main.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Threading;
+using System.Xml.Linq;
+
+namespace EditeurXML
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+        double resultat = 0;    // rapport nb noeuds treeview sur nb noeuds xml (% de la progressbar)
+        double nbnoeud = 0;    // nombre de noeuds dans le treeview
+        double nbnoeudxml = 0; // nombre de noeuds dans le xml
+        TreeNode tNode;
+
+        // Nouveau tread dédié au chargement du xml.
+        // Le fichier xml sera chargé en arrière-plan.
+        // Le tread principal n'étant pas sollicité
+        // cela permet d'éviter le "ne répond pas" de windows.
+        BackgroundWorker bgw1 = new BackgroundWorker();
+
+
+        public delegate void Add(int i);
+        /////////////////////////////////////// FORM LOAD
+        private void main_Load(object sender, EventArgs e)
+        {
+            bgw1.DoWork += new DoWorkEventHandler(backgroundWorkerLoad_DoWork);
+            bgw1.ProgressChanged += new ProgressChangedEventHandler(backgroundWorkerLoad_ProgressChanged);
+            bgw1.RunWorkerCompleted += new RunWorkerCompletedEventHandler(backgroundWorkerLoad_RunWorkerCompleted);
+            bgw1.WorkerReportsProgress = true;
+            labelsur.Visible = false;
+        }
+
+        /////////////////////////////////////// CHARGER LE FICHIER XML DANS LE TREEVIEW
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0; // initialisation
+            nbnoeud = 0;
+            nbnoeudxml = 0;
+            labelsur.Visible = true;
+            bgw1.RunWorkerAsync();  // démarrage du tread secondaire
+        }
+
+        private void FillTree()
+        {
+            try
+            {
+                var settings = new XmlReaderSettings();
+                var doc = new XmlDocument();
+
+                using (var sr = new StreamReader(@textBoxUrl.Text))
+                {
+                    using (var reader = XmlReader.Create(sr, settings))
+                    {
+                        doc.Load(reader);   // chargement du fichier xml
+                        nbnoeudxml = doc.DocumentElement.GetElementsByTagName("*").Count;   // on compte les noeuds du fichier xml
+                        nbnoeudxml++; // +1 car le noeud parent n'est pas pris en compte
+
+                        treeViewXML.Nodes.Clear();  //initialisation du treeview
+                        // on ajoute ensuite les noeuds du xml dans le treeview
+                        // en utilisant le tread secondaire précédement crééé
+                        treeViewXML.Invoke((MethodInvoker)(() => treeViewXML.Nodes.Add(new TreeNode(doc.DocumentElement.Name))));
+                        TreeNode tNode = new TreeNode();
+                        tNode = treeViewXML.Nodes[0];
+                        AddNode(doc.DocumentElement, tNode);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Le chemin d'accès ou le fichier à charger est invalide");
+            }
+        }
+
+        // La fonction AddNode permet d'ajouter un noeud du fichier xml au treeview
+        private void AddNode(XmlNode inXmlNode, TreeNode inTreeNode)
+        {
+            XmlNode xNode;
+            XmlNodeList nodeList;
+            int i = 0;
+            if (inXmlNode.HasChildNodes)
+            {
+                nbnoeud++;  // on compte en même temps le nombre de noeuds du treeview
+                nodeList = inXmlNode.ChildNodes;
+                for (i = 0; i <= nodeList.Count - 1; i++)
+                {
+                    xNode = inXmlNode.ChildNodes[i];
+                    treeViewXML.Invoke((MethodInvoker)(() => inTreeNode.Nodes.Add(new TreeNode(xNode.Name))));
+                    tNode = inTreeNode.Nodes[i];
+                    AddNode(xNode, tNode);
+                    
+                    // On calcule au fur et à mesure le nombre de noeuds du treeview
+                    // sur le nombre de noeuds du xml
+                    // cela nous donne un pourcentage utilisé par la progressbar
+                    resultat = ((nbnoeud) / (nbnoeudxml) * 100);
+                    if (resultat > 100) // mesure de sécurité au cas où la progressbar dépasserais sa valeur max (ça ne devrait pas arriver)
+                    {
+                        resultat = 100;
+                    }
+                    // deux labels nous donnent l'avancement précis du rapport
+                    treeViewXML.Invoke((MethodInvoker)(() => labelnbnoeud.Text = nbnoeud.ToString()));
+                    treeViewXML.Invoke((MethodInvoker)(() =>labelnbnoeudxml.Text = nbnoeudxml.ToString()));
+                    // la progressbar donne l'avancement global
+                    treeViewXML.Invoke((MethodInvoker)(() => progressBar1.Value = Convert.ToInt32(resultat)));
+                }
+            }
+            else
+            {
+                treeViewXML.Invoke((MethodInvoker)(() => inTreeNode.Text = (inXmlNode.OuterXml).Trim()));
+            }
+        }
+        public void Add1(int i)
+        {
+            tNode = treeViewXML.Nodes.Add(i.ToString());
+        }
+        ///////////////////////////////////////////////////////////////////
+        /////////////////////////////////////// PARCOURIR L'ARBORESCENCE DU DISQUE
+        private void button2_Click(object sender, EventArgs e)
+        {
+            treeViewXML.Nodes.Clear();  // initialisation du treeview
+            textBoxUrl.Text = "";       // initialisation du textbox
+            OpenFileDialog openFile = new OpenFileDialog();
+            openFile.DefaultExt = "mif";
+            openFile.Filter = "Fichier XML (*.xml)|*.xml";  // seuls les fichiers .xml apparaissent
+            openFile.ShowDialog();
+            if (openFile.FileNames.Length > 0)
+            {
+                foreach (string filename in openFile.FileNames)
+                {
+                    textBoxUrl.Text = filename;
+                }
+            }
+        }
+        ///////////////////////////////////////////////////////////////////
+        /////////////////////////////////////// EVENEMENT CLIC DROIT SUR UN NOEUD DU TREEVIEW
+        private void treeViewXML_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right) // si clic-droit
+            {
+                treeViewXML.SelectedNode = treeViewXML.GetNodeAt(e.X, e.Y); // le noeud sur la position du clic est selectionné
+
+                if (treeViewXML.SelectedNode != null)
+                {
+                    contextMenuStrip1.Show(treeViewXML, e.Location);    // sinon on selectionne le noeud le plus proche
+                }
+            }
+        }
+        ///////////////////////////////////////////////////////////////////
+        /////////////////////////////////////// MENU CONTEXTUEL CREER RENOMMER SUPPRIMER
+        private void toolStripMenuItemCreer_Click(object sender, EventArgs e)
+        {
+            treeViewXML.SelectedNode.Nodes.Add("Nouveau noeud");    // ajoute un noeud
+            treeViewXML.SelectedNode.Expand();  // déploie son noeud parent
+            treeViewXML.SelectedNode.LastNode.BeginEdit(); // Edition du nom du nouveau noeud
+        }
+        private void toolStripMenuItemRenommer_Click(object sender, EventArgs e)
+        {
+            treeViewXML.LabelEdit = true;
+            treeViewXML.SelectedNode.BeginEdit();  // Edition du nom du noeud        
+        }
+        private void toolStripMenuItemSupprimer_Click(object sender, EventArgs e)
+        {
+            treeViewXML.SelectedNode.Remove();  // supprenssion du noeud
+        }
+        ///////////////////////////////////////////////////////////////////
+        /////////////////////////////////////// BACKGROUNDWORKER
+        private void backgroundWorkerLoad_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // désactivation de tous les contrôles pendant le chargement
+            treeViewXML.Invoke((MethodInvoker)(() => buttonCharger.Enabled = false));
+            treeViewXML.Invoke((MethodInvoker)(() => buttonCharger.Text = "Chargement..."));
+            treeViewXML.Invoke((MethodInvoker)(() => textBoxUrl.Enabled = false));
+            treeViewXML.Invoke((MethodInvoker)(() => buttonParcourir.Enabled = false));
+            treeViewXML.Invoke((MethodInvoker)(() => contextMenuStrip1.Enabled = false));
+
+            FillTree();     //fonction de chargement du fichier xml
+
+            // réactivation des contrôles
+            treeViewXML.Invoke((MethodInvoker)(() => buttonCharger.Text = "Charger"));
+            treeViewXML.Invoke((MethodInvoker)(() => buttonCharger.Enabled = true));
+            treeViewXML.Invoke((MethodInvoker)(() => textBoxUrl.Enabled = true));
+            treeViewXML.Invoke((MethodInvoker)(() => buttonParcourir.Enabled = true));
+            treeViewXML.Invoke((MethodInvoker)(() => contextMenuStrip1.Enabled = true));
+        }
+
+        private void backgroundWorkerLoad_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorkerLoad_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+        }
+        ///////////////////////////////////////////////////////////////////
+
+    }
+}
